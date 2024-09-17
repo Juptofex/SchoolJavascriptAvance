7129d4daa31a62eed6fae110cd6c1c590066a9b6
Julien Damilot <julien.damilot@student.vinci.be>
2024-09-17 15:14:18 +0200

    new: ex1.2

--- tutorials/back/api/basic/app.ts
+++ tutorials/back/api/basic/app.ts
@@ -12,6 +12,35 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+let counter: number = 0;
+let counter2: number = 0;
+let counter3: number = 0;
+let counter4: number = 0;
+const requestCounter: express.RequestHandler = (req, _res, next) => {
+  if (req.method === "GET") {
+    if (req.path === "/") {
+      counter++;
+    }
+    if (req.path === "/pizzas") {
+      counter2++;
+    }
+  }
+  if (req.method === "POST") {
+    counter3++;
+  }
+  if (req.method === "DELETE") {
+    counter4++;
+  }
+  console.log(`Request counter :
+  - GET / : ${counter}
+  - GET /pizzas : ${counter2}
+  - POST /pizzas : ${counter3}
+  - DELETE /pizzas : ${counter4}`);
+  next();
+};
+
+app.use(requestCounter);
+
 app.use("/users", usersRouter);
 app.use("/pizzas", pizzaRouter);
 app.use("/drinks", drinkRouter);
@@ -20,8 +49,8 @@ app.use("/films", filmRouter);
 const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
     console.error(err.stack);
     return res.status(500).send("Something broke!");
-  };
+};
   
-  app.use(errorHandler);  
+app.use(errorHandler);  
 
 export default app;
