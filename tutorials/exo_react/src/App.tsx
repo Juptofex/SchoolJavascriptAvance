feec8fe6f90040b7c427ce9799dbea1fcff521c3
juptofex <julien.damilot@gmail.com>
2024-10-14 12:16:13 +0200

    new: ex2.3

--- tutorials/exo_react/src/App.tsx
+++ tutorials/exo_react/src/App.tsx
@@ -1,12 +1,12 @@
-import './App.css'
+import "./App.css";
 
 interface PageTitleProps {
   title: string;
 }
 
-const PageTitle = ({title}: PageTitleProps) => {
+const PageTitle = ({ title }: PageTitleProps) => {
   return <h1>{title}</h1>;
-}
+};
 
 interface Movie {
   title: string;
@@ -19,7 +19,7 @@ const Movie = ({ title, director }: Movie) => {
       <strong>{title}</strong> - Réalisateur : {director}
     </li>
   );
-}
+};
 
 interface CinemaProps {
   name: string;
@@ -37,38 +37,64 @@ const Cinema = ({ name, movies }: CinemaProps) => {
       </ul>
     </div>
   );
-}
+};
 
 const App = () => {
   const pageTitle = "Informations sur les films dans les cinémas";
 
   const cinema1Name = "UGC DeBrouckère";
-  const cinema1Movies = [
-    { title: "HAIKYU-THE DUMPSTER BATTLE", director: "Susumu Mitsunaka " },
-    { title: "GOODBYE JULIA", director: "Mohamed Kordofani" },
-  ];
 
-  const cinema2Name = "UGC Toison d'Or";
-  const cinema2Movies = [
-    { title: "THE WATCHER", director: "Ishana Night Shyamalan" },
-    { title: "BAD BOYS: RIDE OR DIE", director: "Bilall Fallah" },
-  ];
+  const moviesCinema1 = [
+  {
+    title: "HAIKYU-THE DUMPSTER BATTLE",
+    director: "Susumu Mitsunaka",
+  },
+  {
+    title: "GOODBYE JULIA",
+    director: "Mohamed Kordofani",
+  },
+  {
+    title: "INCEPTION",
+    director: "Christopher Nolan",
+  },
+  {
+    title: "PARASITE",
+    director: "Bong Joon-ho",
+  },
+];
 
-  const cinema3Name = "Kinepolis";
-  const cinema3Movies = [
-    { title: "Flic", director: "Enes Yumer" },
-    { title: "Bruh", director: "Enes Yumer 2e du nom" },
-  ];
+const cinema2Name = "UGC Toison d'Or";
 
+const moviesCinema2 = [
+  {
+    title: "THE WATCHERS",
+    director: "Ishana Night Shyamalan",
+  },
+  {
+    title: "BAD BOYS: RIDE OR DIE",
+    director: "Adil El Arbi, Bilall Fallah",
+  },
+  {
+    title: "TENET",
+    director: "Christopher Nolan",
+  },
+  {
+    title: "THE IRISHMAN",
+    director: "Martin Scorsese",
+  },
+]; 
+
+  
   return (
     <div>
       <PageTitle title={pageTitle} />
-      <Cinema name={cinema1Name} movies={cinema1Movies} />
-      <Cinema name={cinema2Name} movies={cinema2Movies} />
-      <Cinema name={cinema3Name} movies={cinema3Movies} />
+
+      <Cinema name={cinema1Name} movies= {moviesCinema1} />
+
+      <Cinema name={cinema2Name} movies={moviesCinema2} />
     </div>
   );
 };
 
-export default App;
 
+export default App;
